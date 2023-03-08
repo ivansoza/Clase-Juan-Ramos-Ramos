a131e4bef15eb7a04118d22a1e4ab12692ed7633
ArleyIvan <ivansz123@gmail.com>
2023-03-08 17:10:28 -0600

    Nuevas tareas

---
+++ 1000 ejercicios en Python/ex010_n_nn_nnn.py
@@ -0,0 +1,10 @@
+
+#n= 3 3+33+333= 369
+n = input("Escriba el valor de n: ")
+
+nn = int("{}{}".format(n,n))
+nnn = int("%s%s%s"%(n,n,n))
+n = int(n)
+suma = n + nn + nnn
+
+print(f"El resultado es: {suma}")
